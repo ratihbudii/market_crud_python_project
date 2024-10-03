2ae2d3fc1f09c5ec00681e5e9ecac07e7f05f6dd
Ratih <ratih.budi97@gmail.com>
2024-10-03 21:06:25 +0700

    Add Payment Validation

--- main.py
+++ main.py
@@ -25,3 +25,20 @@ print(f"Jeruk: {amount_orange} x {price_orange} = {price_orange_total}")
 print(f"Anggur: {amount_grape} x {price_grape} = {price_grape_total}")
 
 print("Total belanja: ", price_apple_total+price_orange_total+price_grape_total)
+
+# Create ask amount of money
+yangDibayar = int(input("Masukkan jumlah uang: "))
+
+total_beli = price_apple_total + price_orange_total + price_grape_total
+
+# Condiiton 1 2 dan 3
+if yangDibayar < total_beli:
+    selisih_kurang_bayar = total_beli - yangDibayar
+    print("Transaksi anda dibatalkan")
+    print(f"Uang kurang sebesar {selisih_kurang_bayar}")
+elif yangDibayar == total_beli:
+    print("Terima kasih")
+else:
+    lebih_bayar = yangDibayar - total_beli
+    print(f"Uang kembalian Anda sebesar {lebih_bayar}")
+    print("Terima kasih")
